9f97ecb1aad12aecd1295efa4d1e2563a158ccca
melody-story <rnlqls7454@naver.com>
2024-02-07 23:14:21 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Tech Stack
+
+- Java 21
+- Spring Boot 3.2.2
+- Gradle-Groovy
---
+++ src/main/java/melody/project/Message.java
@@ -0,0 +1,14 @@
+package melody.project;
+
+public class Message {
+
+	private String message;
+
+	public Message(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
